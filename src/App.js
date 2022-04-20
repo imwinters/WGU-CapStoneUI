53bec86988d6aba18e475766311e3ef1e1cd69fc
Isaac Winters <IsaacMcPheeWinters@gmail.com>
2022-04-19 18:50:10 -0700

    create UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import Box from '@mui/material/Box';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid'
+import Paper from '@mui/material/Paper'
+import React from 'react';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <AppBar position='sticky'>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            WGU Capstone
+          </Typography>
+          <Typography>
+            Isaac M. Winters
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Grid container>
+        <Grid item>
+          <Paper sx={{
+            height: '80vh',
+            width: '65vw',
+            margin: 1,
+            display: "flex",
+            justifyContent: "center",
+            textAlign: "center",
+            padding: 1
+          }}>
+            <Box
+              component="img"
+              sx={{
+                height: '40vh'
+              }}
+              alt="X-Ray study of a human torso"
+              src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
+            />
+          </Paper>
+        </Grid>
+        <Grid item>
+          <Paper variant="outlined" sx={{ height: '80vh', width: '20vw', margin: 1, padding: 1 }}>
+            Hello
+          </Paper>
+        </Grid>
+      </Grid>
+    </React.Fragment>
+
   );
 }
 
